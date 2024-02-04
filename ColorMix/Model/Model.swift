a0929fcbcdffe01af81b81dbecac3913dc8f8896
Nikita Kornuk <kornyk62@gmail.com>
2024-02-04 16:40:32 +0100

    add depends package and fail swift

---
+++ ColorMix/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  ColorMix
+//
+//  Created by Admin on 04.02.2024.
+//
+
+import Foundation
---
+++ ColorMix/ModelView/ModelView.swift
@@ -0,0 +1,8 @@
+//
+//  ModelView.swift
+//  ColorMix
+//
+//  Created by Admin on 04.02.2024.
+//
+
+import Foundation
