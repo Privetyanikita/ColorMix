d15fc95c5fa55e12cdebde05a282d0c03609f14b
Nikita Kornuk <kornyk62@gmail.com>
2024-02-05 00:32:43 +0100

    Added a color identifier

--- ColorMix/ModelView/ModelView.swift
+++ ColorMix/ModelView/ModelView.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ColorViewModel {
-    var colorData = ColorData(color1: .blue, color2: .red)
+    var colorData = ColorData(color1: .blue, color2: .red, nameColor1: "Blue", nameColor2: "Red")
     var mixedColor: UIColor {
         get {
             var red1: CGFloat = 0, green1: CGFloat = 0, blue1: CGFloat = 0, alpha1: CGFloat = 0
@@ -41,6 +41,14 @@ class ColorViewModel {
         }
         print("Colors loaded")
     }
+    
+    func colorName(from color: UIColor) -> String {
+        if let colorName = color.closestStandardColorName() {
+            return colorName
+        } else {
+            return "Custom Color"
+        }
+    }
 }
 
 extension UserDefaults {
@@ -55,3 +63,48 @@ extension UserDefaults {
     }
 }
 
+extension UIColor {
+    func closestStandardColorName() -> String? {
+        let standardColors: [String: UIColor] = [
+            "Black": .black,
+            "Dark Gray": .darkGray,
+            "Light Gray": .lightGray,
+            "White": .white,
+            "Gray": .gray,
+            "Red": .red,
+            "Green": .green,
+            "Blue": .blue,
+            "Cyan": .cyan,
+            "Yellow": .yellow,
+            "Magenta": .magenta,
+            "Orange": .orange,
+            "Purple": .purple,
+            "Brown": .brown,
+            "Clear": .clear
+        ]
+        
+        var closestColorName: String?
+        var minDistance: CGFloat = .greatestFiniteMagnitude
+        
+        for (name, standardColor) in standardColors {
+            let distance = self.distance(to: standardColor)
+            if distance < minDistance {
+                minDistance = distance
+                closestColorName = name
+            }
+        }
+        
+        return closestColorName
+    }
+    
+    func distance(to color: UIColor) -> CGFloat {
+        var r1: CGFloat = 0, g1: CGFloat = 0, b1: CGFloat = 0, a1: CGFloat = 0
+        var r2: CGFloat = 0, g2: CGFloat = 0, b2: CGFloat = 0, a2: CGFloat = 0
+        
+        self.getRed(&r1, green: &g1, blue: &b1, alpha: &a1)
+        color.getRed(&r2, green: &g2, blue: &b2, alpha: &a2)
+        
+        let distance = sqrt(pow(r1 - r2, 2) + pow(g1 - g2, 2) + pow(b1 - b2, 2))
+        return distance
+    }
+}
