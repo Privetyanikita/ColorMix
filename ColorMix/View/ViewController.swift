50c4967ad3eb18ad5fab16b916f5ffd093fcfc8e
Nikita Kornuk <kornyk62@gmail.com>
2024-02-04 19:56:35 +0100

    add UserDefaults, add logik

--- ColorMix/View/ViewController.swift
+++ ColorMix/View/ViewController.swift
@@ -10,6 +10,7 @@ import SnapKit
 
 final class ViewController: UIViewController {
     
+    // MARK: - Properties
     private let colorView1:UIView = {
        let element = UIView()
         element.tag = 1
@@ -40,7 +41,10 @@ final class ViewController: UIViewController {
     }()
     
     var selectedView: UIView?
+    
+    private let userDefaults = UserDefaults.standard
 
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -48,7 +52,19 @@ final class ViewController: UIViewController {
         setupConstrains()
         addAction()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadColors()
+        updateColorResultView()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        saveColors()
+    }
 
+    //MARK: - PrivateMethods
     private func setupViews(){
         view.addSubview(colorView1)
         view.addSubview(colorView2)
@@ -61,6 +77,53 @@ final class ViewController: UIViewController {
         colorView2.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(changeColor)))
     }
     
+    private func calculateMixedColor() -> UIColor? {
+        guard let color1 = colorView1.backgroundColor, let color2 = colorView2.backgroundColor else { return nil }
+        
+        var red1: CGFloat = 0, green1: CGFloat = 0, blue1: CGFloat = 0, alpha1: CGFloat = 0
+        color1.getRed(&red1, green: &green1, blue: &blue1, alpha: &alpha1)
+        
+        var red2: CGFloat = 0, green2: CGFloat = 0, blue2: CGFloat = 0, alpha2: CGFloat = 0
+        color2.getRed(&red2, green: &green2, blue: &blue2, alpha: &alpha2)
+        
+        let mixedRed = (red1 + red2) / 2
+        let mixedGreen = (green1 + green2) / 2
+        let mixedBlue = (blue1 + blue2) / 2
+        
+        return UIColor(red: mixedRed, green: mixedGreen, blue: mixedBlue, alpha: 1.0)
+    }
+    
+    private func updateColorResultView() {
+        if let mixedColor = calculateMixedColor() {
+            colorResultView.backgroundColor = mixedColor
+        }
+    }
+    
+    private func saveColors() {
+        guard let color1 = colorView1.backgroundColor, let color2 = colorView2.backgroundColor else { return }
+        
+        let color1Data = try? NSKeyedArchiver.archivedData(withRootObject: color1, requiringSecureCoding: false)
+        let color2Data = try? NSKeyedArchiver.archivedData(withRootObject: color2, requiringSecureCoding: false)
+        
+        userDefaults.set(color1Data, forKey: "color1")
+        userDefaults.set(color2Data, forKey: "color2")
+        print("Colors saved")
+    }
+
+    private func loadColors() {
+        if let color1Data = userDefaults.data(forKey: "color1"),
+           let color1 = try? NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: color1Data) {
+            colorView1.backgroundColor = color1
+        }
+        
+        if let color2Data = userDefaults.data(forKey: "color2"),
+           let color2 = try? NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: color2Data) {
+            colorView2.backgroundColor = color2
+        }
+        print("Colors loaded")
+    }
+    
+    //MARK: - objc Function
     @objc private func changeColor(_ sender: UITapGestureRecognizer){
         guard let view = sender.view else { return }
         let vc = UIColorPickerViewController()
@@ -71,7 +134,21 @@ final class ViewController: UIViewController {
         selectedView = view
         present(vc, animated: true)
     }
-    
+
+}
+
+//MARK: - UIColorPickerViewControllerDelegate
+extension ViewController: UIColorPickerViewControllerDelegate {
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        guard let selectedView = selectedView else { return }
+        let color = viewController.selectedColor
+        selectedView.backgroundColor = color
+        updateColorResultView()
+    }
+}
+
+//MARK: - SetupConstrains
+extension ViewController{
     private func setupConstrains(){
         colorView1.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).inset(50)
@@ -90,21 +167,6 @@ final class ViewController: UIViewController {
             make.centerX.equalToSuperview()
             make.width.height.equalTo(100)
         }
-    }
-
-}
-
-extension ViewController: UIColorPickerViewControllerDelegate {
-    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
-        guard let selectedView = selectedView else { return }
-        let color = viewController.selectedColor
-        selectedView.backgroundColor = color
-    }
-    
-    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
-        guard let selectedView = selectedView else { return }
-        let color = viewController.selectedColor
-        selectedView.backgroundColor = color
     }
 }
 
